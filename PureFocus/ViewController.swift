519a69e20a9a02880b3fbff5eb7544bcb018da63
Ryan Dines <ryan.dines@gmail.com>
2017-08-18 14:24:44 -0400

    Initial Commit

---
+++ PureFocus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PureFocus
+//
+//  Created by Ryan Dines on 8/18/17.
+//  Copyright © 2017 Ryan Dines. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
